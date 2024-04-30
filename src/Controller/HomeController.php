f7913c66fd0d12e55f08800d5f6be5051b59180d
Alice Marique <mariquealice83@gmail.com>
2024-04-30 14:44:12 +0200

    soumettre un e question formulaire et connection a la database

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,6 +12,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+
         $questions = [
             [
                 'id' => '1',
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,25 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Form\QuestionType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'question_form')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface  $em): Response
     {
-        $formQuestion = $this->createForm(QuestionType::class);
+        $question = new Question();
+        $formQuestion = $this->createForm(QuestionType::class, $question);
 
         $formQuestion->handleRequest($request);
 
         if($formQuestion->isSubmitted() && $formQuestion->isValid()){
+            $question->setNbrOfResponse(0);
+            $question->setRating(0);
+            $question->setCreatedAt(new \DateTimeImmutable());
+            $em->persist($question);
+            $em->flush();
+            $this->addFlash('success','Votre question a été ajoutée');
+
+            return $this->redirectToRoute('home');
 
         }
-
 
         return $this->render('question/index.html.twig', [
             'form' => $formQuestion->createView(),
