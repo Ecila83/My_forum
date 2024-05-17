ec1676bca217f5db79e802c4b825c8aa1e236694
Alice Marique <mariquealice83@gmail.com>
2024-05-17 14:37:09 +0200

    tobar deconnexion + protection des route

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -12,27 +12,30 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class QuestionController extends AbstractController
 {
     #[Route('/question/ask', name: 'question_form')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function index(Request $request, EntityManagerInterface  $em): Response
     {
+        $user =$this->getUser();
         $question = new Question();
         $formQuestion = $this->createForm(QuestionType::class, $question);
 
         $formQuestion->handleRequest($request);
 
-        if($formQuestion->isSubmitted() && $formQuestion->isValid()){
+        if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
             $question->setNbrOfResponse(0);
             $question->setRating(0);
+            $question->setAuthor($user);
             $question->setCreatedAt(new \DateTimeImmutable());
             $em->persist($question);
             $em->flush();
-            $this->addFlash('success','Votre question a été ajoutée');
+            $this->addFlash('success', 'Votre question a été ajoutée');
 
             return $this->redirectToRoute('home');
-
         }
 
         return $this->render('question/index.html.twig', [
@@ -41,31 +44,39 @@ class QuestionController extends AbstractController
     }
 
     #[Route('/question/{id}', name: 'question_show')]
-    public function show(Request $request,Question $question,EntityManagerInterface $em): Response
+    public function show(Request $request, Question $question, EntityManagerInterface $em): Response
     {
-        $comment = new Comment();
-        $commentForm = $this->createForm(CommentType::class,$comment);
-        $commentForm->handleRequest($request);
-        if ($commentForm->isSubmitted() && $commentForm->isValid()){
-            $comment->setCreatedAt(new \DateTimeImmutable());
-            $comment->setRating(0);
-            $comment->setQuestion($question);
-            $question->setNbrOfResponse($question->getNbrOfResponse() + 1);
-            $em->persist($comment);
-            $em->flush();
-            $this->addFlash('success', 'Votre reponse à bien été ajoutée');
-
-            return $this->redirect($request->getUri());
-
-        }
-    
-        return $this->render('question/show.html.twig', [
+        $options = [
             'question' => $question,
-            'form' => $commentForm->createView()
-        ]);
+
+        ];
+        $user = $this->getUser();
+        if ($user) {
+            $comment = new Comment();
+            $commentForm = $this->createForm(CommentType::class, $comment);
+            $commentForm->handleRequest($request);
+            if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+                $comment->setCreatedAt(new \DateTimeImmutable());
+                $comment->setRating(0);
+                $comment->setQuestion($question);
+                $comment->setAuthor($user);
+                $question->setNbrOfResponse($question->getNbrOfResponse() + 1);
+                $em->persist($comment);
+                $em->flush();
+                $this->addFlash('success', 'Votre reponse à bien été ajoutée');
+
+                return $this->redirect($request->getUri());
+            }
+            $options['form'] = $commentForm->createView();
+        }
+
+        return $this->render('question/show.html.twig', $options);
     }
+
     #[Route('/question/rating/{id}/{score}', name: 'question_rating')]
-    public function ratingQuestion(Request $request,Question $question, int $score, EntityManagerInterface $em){
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function ratingQuestion(Request $request, Question $question, int $score, EntityManagerInterface $em)
+    {
         $question->setRating($question->getRating() + $score);
         $em->flush();
         $referer = $request->server->get('HTTP_REFERER');
@@ -73,7 +84,9 @@ class QuestionController extends AbstractController
     }
 
     #[Route('/comment/rating/{id}/{score}', name: 'comment_rating')]
-    public function ratingComment(Request $request,Comment $comment, int $score, EntityManagerInterface $em){
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function ratingComment(Request $request, Comment $comment, int $score, EntityManagerInterface $em)
+    {
         $comment->setRating($comment->getRating() + $score);
         $em->flush();
         $referer = $request->server->get('HTTP_REFERER');
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,25 +7,37 @@ use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authenticator\FormLoginAuthenticator;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+
+
 
 class SecurityController extends AbstractController
 {
+
+    public function __construct(
+        private FormLoginAuthenticator $authenticator
+    ) {
+  
+    }
+
     #[Route('/signup', name: 'signup')]
-    public function signup(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
+    public function signup(UserAuthenticatorInterface $userAuthenticator, Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher)
     {
         $user =new User();
         $userForm = $this->createForm(UserType::class, $user);
         $userForm->handleRequest($request);
-        if($userForm->isSubmitted() && $userForm->isValid()){
-            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $hash = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hash);
             $em->persist($user);
             $em->flush();
             $this->addFlash('sucess', 'Bienvenue sur My Forum !');
-            return $this->redirectToRoute('login');
+            return $userAuthenticator->authenticateUser($user, $this->authenticator, $request);
         }
 
         return $this->render('security/signup.html.twig', [
