3e7bd593f780d1e8ce5b4e80f2c4c8524f5aa39d
Alice Marique <mariquealice83@gmail.com>
2024-05-17 15:34:27 +0200

    user controller +redirection profil

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -244,4 +244,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getFullname(): ?string
+    {
+        return $this->firstname . ' '. $this->lastname;
+    }
 }
